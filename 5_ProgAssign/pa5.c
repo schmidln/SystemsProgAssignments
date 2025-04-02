d85399f758758bc6a65e5c58184a8f5931dc0bc5
schmidln <schmidln@bc.edu>
2025-04-01 20:12:43 -0400

    main function updates

--- 5_ProgAssign/pa5.c
+++ 5_ProgAssign/pa5.c
@@ -4,23 +4,39 @@ Lucas Schmidt - schmidln@bc.edu
 Andy Bello - belloac@bc.edu
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int show_hidden = 0;
 
-int main(int argc, char **argv) {
-    if (argc == 1) {
-        show_hidden = 0;
-    }
-    
-    else if (argc == 2) {
-        if (argv[1] == "-l") {
-            show_hidden = 1;
-        }
-        else {
-            return 1;
-        }
+void walk_dir(char *dir, int indent) {
+    // To be implemented
+}
 
+int main(int argc, char **argv) {
+    if (argc == 2) {
+        if (strcmp(argv[1], "-l") == 0) {
+            // "-l" is not valid by itself
+            return EXIT_FAILURE;
+        } else {
+            walk_dir(argv[1], 0);
+            return EXIT_SUCCESS;
+        }
     }
+
+    else if (argc == 3) {
+        if (strcmp(argv[2], "-l") == 0) {
+            show_hidden = 1;
+            walk_dir(argv[1], 0);
+            return EXIT_SUCCESS;
+        } else {
+            return EXIT_FAILURE;
+        }
+    }
+
     else {
-        return 1;
+        // Either no args or too many
+        return EXIT_FAILURE;
     }
 }
