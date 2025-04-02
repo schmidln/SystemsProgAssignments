a2ba8c2d3f4d29fb093b8320e2f6ce7e604eb5b9
Andy Bello <belloandy56@gmail.com>
2025-04-02 00:10:52 -0400

    Changes
    changes

--- 5_ProgAssign/tree.c
+++ 5_ProgAssign/tree.c
@@ -1,13 +1,11 @@
 /* 
-Authors: 
-Lucas Schmidt - schmidln@bc.edu
-Andy Bello - belloac@bc.edu
+Authors: Lucas Schmidt - schmidln@bc.edu, Andy Bello - belloac@bc.edu
 */
 
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <dirent.h>
 
 int show_hidden = 0;
 
@@ -16,18 +14,15 @@ int is_pwd_or_parent(char *filename) {
 }
 
 void indent(int indent) {
-    for (int i = 0; i < indent; i++) {
-        putchar(' ');
-        putchar(' ');
-        putchar(' ');
+    for (int i = 0; i < indent * 4; i++) {
         putchar(' ');
     }
 }
 
-void walk_dir(char * dir, int indent) {
+void walk_dir(char * dir, int indent_level) {
     DIR * dp = opendir(dir);
     if (dp == NULL) {
-        perror("opendir");
+        perror("Error opening file");
         return;
     }
 
@@ -36,33 +31,33 @@ void walk_dir(char * dir, int indent) {
     while ((entry = readdir(dp)) != NULL) {
         if (is_pwd_or_parent(entry -> d_name)) {
             continue;
-        }
+        } 
         if (entry -> d_name[0] == 46 && show_hidden == 0) {
             continue;
-        }
+        } 
+
         if (entry -> d_type == DT_DIR) {
             printf("%s", entry -> d_name);
             putchar(':');
             putchar('\n');
-            walk_dir(entry -> d_name, indent + 1);
+            size_t buffer_length = strlen(dir) + 1 + strlen(entry -> d_name) + 1;
+            char buffer[buffer_length];
+            snprintf(buffer, buffer_length, "%s/%s", dir, entry -> d_name);
+            walk_dir(buffer, indent_level + 1); 
+        } else {
+            indent(indent_level);
+            printf("%s\n", entry -> d_name);   
         }
-        indent(indent);
-        printf("%s\n", entry -> d_name);
     }
+
+    closedir(dp);
 }
 
 int main(int argc, char ** argv) {
     if (argc == 2) {
-        if (strcmp(argv[1], "-l") == 0) {
-            // "-l" is not valid by itself
-            return EXIT_FAILURE;
-        } else {
-            walk_dir(argv[1], 0);
-            return EXIT_SUCCESS;
-        }
-    }
-
-    else if (argc == 3) {
+        walk_dir(argv[1], 0);
+        return EXIT_SUCCESS;
+    } else if (argc == 3) {
         if (strcmp(argv[2], "-l") == 0) {
             show_hidden = 1;
             walk_dir(argv[1], 0);
@@ -70,9 +65,7 @@ int main(int argc, char ** argv) {
         } else {
             return EXIT_FAILURE;
         }
-    }
-
-    else {
+    } else {
         return EXIT_FAILURE;
     }
 }
