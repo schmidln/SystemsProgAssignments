ad9d926c4e1887d6601951ece1c792c7df19bee5
andybello28 <108368351+andybello28@users.noreply.github.com>
2025-05-01 21:28:45 -0400

    Update matrix_mult.c

--- 8_ProgAssign/matrix_mult.c
+++ 8_ProgAssign/matrix_mult.c
@@ -1,18 +1,23 @@
-/* Amaan Banga - bangaa@bc.edu
- * April 28, 2024
- * PA8
- */ 
-
-#include "matrix_mult.h"
-
 void init_matrix(double * matrix, int dim) {
-    double k = 1.0;
+    double k = 1;
     for (int i = 0; i < dim; i++) {
         for (int j = 0; j < dim; j++) {
-            matrix[i * dim + j] = k++;
+            matrix[i * dim + j] = k;
+            k++;
         }
     }
 }
+
+
+void print_matrix(double * matrix, int dim) {
+    for (int i = 0; i < dim; i++) {
+        for (int j = 0; j < dim; j++) {
+            printf("%f ", matrix[i * dim + j]);
+        }
+        putchar('\n');
+    }
+}
+
 void multiply_serial(const double * const a, const double * const b, double * const c, const int dim, const int num_workers) {
     for (int i = 0; i < dim; i++) {
         for (int j = 0; j < dim; j++) {
@@ -23,85 +28,146 @@ void multiply_serial(const double * const a, const double * const b, double * co
         }
     }
 }
-void print_matrix(double * matrix, int dim) {
-    for (int i = 0; i < dim; i++) {
+
+void multiply_chunk(const double * const a, const double * const b, double * const c, const int dim, const int start, int chunk) {
+    for (int i = start; i < start + chunk; i++) {
         for (int j = 0; j < dim; j++) {
-            printf("%f ", matrix[i * dim + j]);
-        }
-        printf("\n");
-    }
-}
-void * mmap_checked(size_t size) {
-    void * ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    if (ptr == MAP_FAILED) {
-        perror("Mapping failed");
-        exit(EXIT_FAILURE);
-    }
-    return ptr;
-}
-void munmap_checked(void * map, size_t size) {
-    if (munmap(map, size) == -1) {
-        perror("Munmap has occured");
-        exit(EXIT_FAILURE);
-    }
-}
-pid_t fork_checked() {
-    pid_t process_id = fork();
-    if (process_id == -1) {
-        perror("Fork Failed");
-        exit(EXIT_FAILURE);
-    }
-    return process_id;
-}
-void multiply_chunk(const double * const a, const double * const b, double * const c, const int dim, const int num_workers, const int row_start) {
-    int chunk_size = dim / num_workers;
-    int row_end = (row_start + chunk_size >= dim) ? dim : row_start + chunk_size;
-    for (int i = row_start; i < row_end; i++) {
-        for (int j = 0; j < dim; j++) {
-            c[i * dim + j] = 0.0;
+            c[i * dim + j] = 0;
             for (int k = 0; k < dim; k++) {
                 c[i * dim + j] += a[i * dim + k] * b[k * dim + j];
             }
         }
     }
 }
+
+void * mmap_checked(size_t size) {
+    void * mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    if (mem == MAP_FAILED) {
+        perror("Demand-zero memory allocation failure");
+        exit(EXIT_FAILURE);
+    }
+    return mem;
+}
+
+void munmap_checked(void * map, size_t size) {
+    if (munmap(map, size) == -1) {
+        perror("Munmap failure");
+        exit(EXIT_FAILURE);
+    }
+}
+
+pid_t fork_checked() {
+    pid_t process_id = fork();
+    if (process_id == -1) {
+        perror("Fork failure");
+        exit(EXIT_FAILURE);
+    }
+    return process_id;
+}
+
 void multiply_parallel_processes(const double * const a, const double * const b, double * const c, const int dim, const int num_workers) {
-    double * actual_c = (double *)mmap_checked(dim * dim * sizeof(double));
     int num_procs = num_workers - 1;
     int chunk_size = dim / num_workers;
+    double * shared = (double *)mmap_checked(dim * dim * sizeof(double));
     for (int i = 0; i < num_procs; i++) {
-        pid_t process_id = fork_checked();
-        if (process_id == 0) {
-            multiply_chunk(a, b, actual_c, dim, num_workers, i * chunk_size);
+        int start_row = i * chunk_size;
+        pid_t pid = fork_checked();
+        if (pid == 0) {
+            multiply_chunk(a, b, shared, dim, start_row, chunk_size);
             exit(EXIT_SUCCESS);
         }
     }
-    multiply_chunk(a, b, actual_c, dim, num_workers, num_procs * chunk_size);
+    int start_row = num_procs * chunk_size;
+    multiply_chunk(a, b, shared, dim, start_row, dim - start_row);
     int waiting;
     while (wait(&waiting) > 0);
     for (int i = 0; i < dim * dim; i++) {
-        c[i] = actual_c[i];
+        c[i] = shared[i];
     }
-    munmap_checked(actual_c, dim * dim * sizeof(double));
+    munmap_checked(shared, dim * dim * sizeof(double));
 }
+
+int verify(const double * const m1, const double * const m2, const int dim) {
+    for (int i = 0; i < dim * dim; i++) {
+        if (m1[i] != m2[i]) {
+            return FAILURE;
+        }
+    }
+    return SUCCESS;
+}
+
+struct timeval time_diff(struct timeval * start, struct timeval * end) {
+    struct timeval diff;
+    diff.tv_sec = end->tv_sec - start->tv_sec;
+    diff.tv_usec = end->tv_usec - start->tv_usec;
+    if (diff.tv_usec < 0) {
+        diff.tv_sec--;
+        diff.tv_usec += 1000000;
+    }
+    return diff;
+}
+
+void print_elapsed_time(struct timeval * start, struct timeval * end, const char * const name) {
+    struct timeval diff = time_diff(start, end);
+    int num_workers = 0;
+    if (strcmp(name, "serial") == 0) {
+        num_workers = 1;
+    } else {
+        num_workers = NUM_WORKERS;
+    }
+    const char * microseconds_string = (diff.tv_usec == 1) ? "microsecond" : "microseconds";
+    const char * seconds_string = (diff.tv_sec == 1) ? "second" : "seconds";
+    const char * worker_string = (num_workers == 1) ? "worker" : "workers";
+    printf("Algorithm: %s with %d %s.\n", name, num_workers, worker_string);
+    printf("Time elapsed for %s: %ld %s and %ld %s.\n", name, (long)diff.tv_sec, seconds_string, (long)diff.tv_usec, microseconds_string);
+}
+
+void print_verification(const double * const m1, const double * const m2, const int dim, const char * const name) {
+    int verification = verify(m1, m2, dim);
+    const char * verify_result = (verification == 0) ? "success" : "failure";
+    printf("Verification for %s: %s.\n", name, verify_result);
+}
+
+void run_and_time(
+        multiply_function multiply_matrices,
+        const double * const a,
+        const double * const b,
+        double * const c,
+        const double * const gold,
+        const int dim,
+        const char * const name,
+        const int num_workers,
+        const bool verify
+        ) {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    multiply_matrices(a, b, c, dim, num_workers);
+    gettimeofday(&end, NULL);
+    print_elapsed_time(&start, &end, name);
+    if (verify) {
+        print_verification(c, gold, dim, name);
+    }
+}
+
 void * task(void * arg) {
     Args * args = (Args *)arg;
-    multiply_chunk(args->a, args->b, args->c, args->dim, args->num_workers, args->row_start);
+    multiply_chunk(args->a, args->b, args->c, args->dim, args->row_start, args->chunk);
     return NULL;
 }
+
 void multiply_parallel_threads(const double * const a, const double * const b, double * const c, const int dim, const int num_workers) {
     int num_threads = num_workers - 1;
     pthread_t threads[num_threads];
-    Args arg_set[num_workers];
     int chunk = dim / num_workers;
+    Args arg_set[num_workers];
     int row_start = 0;
     for (int i = 0; i < num_workers; ++i) {
         arg_set[i].a = a;
         arg_set[i].b = b;
         arg_set[i].c = c;
         arg_set[i].dim = dim;
-        arg_set[i].num_workers = num_workers;
         arg_set[i].row_start = row_start;
+        arg_set[i].chunk = chunk;
         row_start += chunk;
     }
     int id = 0;
@@ -114,60 +180,3 @@ void multiply_parallel_threads(const double * const a, const double * const b, d
         pthread_join(threads[i], NULL);
     }
 }
-int verify(const double * const m1, const double * const m2, const int dim) {
-    for (int i = 0; i < dim * dim; i++) {
-        if (m1[i] != m2[i]) {
-            return FAILURE;
-        }
-    }
-    return SUCCESS;
-}
-struct timeval time_diff(struct timeval * start, struct timeval * end) {
-    struct timeval diff;
-    diff.tv_sec = end->tv_sec - start->tv_sec;
-    diff.tv_usec = end->tv_usec - start->tv_usec;
-    if (diff.tv_usec < 0) {
-        diff.tv_sec--;
-        diff.tv_usec += 1000000; 
-    }
-    return diff;
-}
-void print_elapsed_time(struct timeval * start, struct timeval * end, const char * const name) {
-    struct timeval diff = time_diff(start, end);
-    int num_workers = 0;
-    if (strcmp(name, "serial") == 0) {
-        num_workers = 1;
-    } else {
-        num_workers = NUM_WORKERS;
-    }
-    const char * worker_plural = (num_workers == 1) ? "worker" : "workers";
-    const char * seconds_unit = (diff.tv_sec == 1) ? "second" : "seconds";
-    const char * microseconds_unit = (diff.tv_usec == 1) ? "microsecond" : "microseconds";
-    printf("Algorithm: %s with %d %s.\n", name, num_workers, worker_plural);
-    printf("Time elapsed for %s: %ld %s and %ld %s.\n", name, (long)diff.tv_sec, seconds_unit, (long)diff.tv_usec, microseconds_unit);
-}
-void print_verification(const double * const m1, const double * const m2, const int dim, const char * const name) {
-    int verification = verify(m1, m2, dim);
-    const char * check =  (verification == 0) ? "success" : "failure";
-    printf("Verification for %s: %s.\n", name, check);
-}
-void run_and_time(
-        multiply_function multiply_matrices,
-        const double * const a,
-        const double * const b,
-        double * const c,
-        const double * const gold,
-        const int dim,
-        const char * const name,
-        const int num_workers,
-        const bool verify) {
-    struct timeval start, end;
-    gettimeofday(&start, NULL);
-    multiply_matrices(a, b, c, dim, num_workers);
-    gettimeofday(&end, NULL);
-    print_elapsed_time(&start, &end, name);
-    if (verify) {
-        print_verification(c, gold, dim, name);
-    }
-}
-
